ff808f2d83ee5afb9a8e7231c6bc0e86f99edb73
Aidai Mashrapova <aidaimashrapova8@mail.ru>
2025-06-08 22:04:53 +0600

    touch turn added

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const url = 'book.pdf';
 const container = document.getElementById('flipbook');
 
-// Проверим, мобильное ли устройство
+// Проверка: мобильное устройство?
 const isMobile = window.innerWidth <= 900;
 
+// Подгрузка PDF
 pdfjsLib.getDocument(url).promise.then(pdf => {
     const numPages = pdf.numPages;
     const pages = [];
 
+    // Загрузка одной страницы
     const loadPage = pageNum => {
         return pdf.getPage(pageNum).then(page => {
             const viewport = page.getViewport({ scale: 2 });
-
             const canvas = document.createElement('canvas');
             const context = canvas.getContext('2d');
 
@@ -30,6 +31,7 @@ pdfjsLib.getDocument(url).promise.then(pdf => {
         });
     };
 
+    // Загрузка всех страниц
     const loadAll = [];
     for (let i = 1; i <= numPages; i++) {
         loadAll.push(loadPage(i));
@@ -38,6 +40,7 @@ pdfjsLib.getDocument(url).promise.then(pdf => {
     Promise.all(loadAll).then(() => {
         pages.forEach(wrapper => container.appendChild(wrapper));
 
+        // Инициализация turn.js
         $('#flipbook').turn({
             width: isMobile ? window.innerWidth * 0.9 : 1686,
             height: isMobile ? 600 : 600,
@@ -46,5 +49,32 @@ pdfjsLib.getDocument(url).promise.then(pdf => {
             elevation: 50,
             gradients: true
         });
+
+        // Включаем свайп на мобильных
+        if (isMobile) {
+            enableTouchSwipe(document.getElementById('flipbook'));
+        }
     });
 });
+
+// Функция свайпа
+function enableTouchSwipe(flipbook) {
+    let startX = 0;
+
+    flipbook.addEventListener('touchstart', (e) => {
+        startX = e.touches[0].clientX;
+    });
+
+    flipbook.addEventListener('touchend', (e) => {
+        const endX = e.changedTouches[0].clientX;
+        const deltaX = endX - startX;
+
+        if (Math.abs(deltaX) > 50) {
+            if (deltaX < 0) {
+                $('#flipbook').turn('next');
+            } else {
+                $('#flipbook').turn('previous');
+            }
+        }
+    });
+}
